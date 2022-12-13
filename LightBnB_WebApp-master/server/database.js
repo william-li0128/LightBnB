e6d939549f980e56b9cdf86f60581a13b0e66e7d
William Li <110069721+william-li0128@users.noreply.github.com>
2022-12-13 00:15:30 -0800

    connect getAllReservations function with database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -86,7 +86,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT reservations.*, properties.*, avg(rating) as average_rating
+            FROM reservations
+            JOIN properties ON reservations.property_id = properties.id
+            JOIN property_reviews ON properties.id = property_reviews.property_id
+            WHERE reservations.guest_id = $1
+            GROUP BY properties.id, reservations.id
+            ORDER BY reservations.start_date
+            LIMIT $2;`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
